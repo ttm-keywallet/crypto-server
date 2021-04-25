859424b49732863f601b8f415935ff201d6e1205
sa.zheldokas <sa.zheldokas@gmail.com>
2021-04-25 18:51:34 +0300

    Dockerfile

--- common/types/twdata.go
+++ common/types/twdata.go
@@ -3,11 +3,12 @@ package types
 // #cgo CFLAGS: -I../../wallet-core/include
 // #cgo LDFLAGS: -L../../wallet-core/build -L../../wallet-core/build/trezor-crypto -lTrustWalletCore -lprotobuf -lTrezorCrypto -lc++ -lm
 // #include <TrustWalletCore/TWData.h>
+// #include <TrustWalletCore/TWString.h>
 import "C"
 
 import (
-	"unsafe"
 	"encoding/hex"
+	"unsafe"
 )
 
 // C.TWData -> Go byte[]
@@ -28,4 +29,17 @@ func TWDataCreateWithGoBytes(d []byte) unsafe.Pointer {
 // C.TWData -> Go hex string
 func TWDataHexString(d unsafe.Pointer) string {
 	return hex.EncodeToString(TWDataGoBytes(d))
+}
+
+// C.TWString -> Go string
+func TWStringGoString(s unsafe.Pointer) string {
+	return C.GoString(C.TWStringUTF8Bytes(s))
+}
+
+// Go string -> C.TWString
+func TWStringCreateWithGoString(s string) unsafe.Pointer {
+	cStr := C.CString(s)
+	defer C.free(unsafe.Pointer(cStr))
+	str := C.TWStringCreateWithUTF8Bytes(cStr)
+	return str
 }
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/ttmbank/crypto-server/app"
 )
 
 func main() {
 	srv := new(app.App)
 	srv.Initialize()
-	srv.Run(":8020")
-
 	fmt.Println("Starting crypto server")
+	srv.Run(":8020")
 }
